043f7a6c77163b3d691d73f015caf964fd7f62a5
pratikkadam22 <pratikkadam2212@gmail.com>
2020-01-29 15:15:50 -0800

    Test added

--- airline/src/test/java/edu/pdx/cs410J/pkadam/Project1Test.java
+++ airline/src/test/java/edu/pdx/cs410J/pkadam/Project1Test.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410J.pkadam;
 
+import com.sun.tools.javac.Main;
 import edu.pdx.cs410J.AbstractAirline;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import edu.pdx.cs410J.ParserException;
@@ -67,27 +68,33 @@ public class Project1Test extends InvokeMainTestCase {
     }
 
     @Test
-    public void createnewairline(){
-//        File file = new File("pratik.txt");
-//        assertFalse(!file.exists());
-        MainMethodResult result = invokeMain(new String[] {"-textFile","pratik.txt","-print","emirates","123","pdx","03/03/2017","12:00","dbo","09/09/2017","16:00"});
+    public void testnewairline(){
+        MainMethodResult result = invokeMain(new String[] {"-textFile", "pratik.txt", "emirates", "123", "pdx", "03/03/2017", "12:00", "dbo", "09/09/2017", "16:00"});
         assertThat(result.getExitCode(), equalTo(0));
-//        assertTrue(file.exists());
-//        TextParser parser = new TextParser();
-//        parser.setnames("pratik.txt", "emirates");
-//        AbstractAirline parsedairline = parser.parse();
-//        String expected = "Flight 123 departs pdx at 03/03/2017 12:00 arrives dbo at 09/09/2017 16:00";
-//        assertTrue(parsedairline.getFlights().toString().contains(expected));
-//        assertThat(result.getTextWrittenToStandardError(), containsString("Please check the arguments"));
     }
-//
+
 //    @Test
-//    public void filecontentformatiswrong() throws FileNotFoundException {
-//        File file = new File("pratik.txt");
-//        PrintWriter out = new PrintWriter(file);
-//        out.write("ageaesdgdgea\ndfdsfadsdf\nsdaff");
-//        MainMethodResult result = invokeMain(new String[] {"-textFile", "pratik.txt", "-print", "emirates", "123", "pdx", "03/03/2017", "12:00", "dbo", "09/09/2017", "16:00"});
-//        assertThat(result.getExitCode(), equalTo(1));
-//        assertThat(result.getTextWrittenToStandardError(), containsString("The text file is not formatted properly."));
+//    public void createnewairline(){
+////        File file = new File("pratik.txt");
+////        assertFalse(!file.exists());
+//        MainMethodResult result = invokeMain(new String[] {"-textFile","pratik.txt","emirates","123","pdx","03/03/2017","12:00","dbo","09/09/2017","16:00"});
+//        assertThat(result.getExitCode(), equalTo(0));
+////        assertTrue(file.exists());
+////        TextParser parser = new TextParser();
+////        parser.setnames("pratik.txt", "emirates");
+////        AbstractAirline parsedairline = parser.parse();
+////        String expected = "Flight 123 departs pdx at 03/03/2017 12:00 arrives dbo at 09/09/2017 16:00";
+////        assertTrue(parsedairline.getFlights().toString().contains(expected));
+////        assertThat(result.getTextWrittenToStandardError(), containsString("Please check the arguments"));
 //    }
+////
+////    @Test
+////    public void filecontentformatiswrong() throws FileNotFoundException {
+////        File file = new File("pratik.txt");
+////        PrintWriter out = new PrintWriter(file);
+////        out.write("ageaesdgdgea\ndfdsfadsdf\nsdaff");
+////        MainMethodResult result = invokeMain(new String[] {"-textFile", "pratik.txt", "-print", "emirates", "123", "pdx", "03/03/2017", "12:00", "dbo", "09/09/2017", "16:00"});
+////        assertThat(result.getExitCode(), equalTo(1));
+////        assertThat(result.getTextWrittenToStandardError(), containsString("The text file is not formatted properly."));
+////    }
 }
